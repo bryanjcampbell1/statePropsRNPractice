f35d45ff9432f9eed4439bd3bf9e7202784175af
Bryan Campbell <bryancampbell@Bryans-MacBook-Pro.local>
2019-05-08 16:29:30 -0400

    state works. no props yet

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Button, Alert, AppRegistry, Text } from 'react-native';
+
+
+export default class App extends React.Component {
+  render() {
+    return < MainApp/>;
+  }
+}
+
+class MainApp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { tapNumber: 0 };
+  }
+
+handleClick = () => {
+      // your logic
+      this.setState({
+      tapNumber: 1,
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button onPress={this.handleClick} title="Click Me" color="#FFFFFF" accessibilityLabel="Tap on Me" />
+          <Text>Tap Number is {this.state.tapNumber}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF'
+  },
+  buttonContainer: {
+    backgroundColor: '#2E9298',
+    borderRadius: 10,
+    padding: 10,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 10,
+    shadowOpacity: 0.25
+  }
+})
+
